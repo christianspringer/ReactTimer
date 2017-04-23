83b261e0c934ff26815e24f7a975141d2100c1f8
christian <cfs2228@gmail.com>
2017-04-23 17:23:17 -0400

    fleshed out the countdown tests

--- app/tests/components/countdown.test.jsx
+++ app/tests/components/countdown.test.jsx
@@ -9,6 +9,30 @@ var Countdown = require('Countdown');
 describe('Countdown', ()=>{
     it('should exist', () =>{
         expect(Countdown).toExist();
-    })
+    });
+
+    describe('handleSetCountdown', ()=>{
+        it('should set state to started and countdown', (done)=>{
+            var countdown = TestUtils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountdown(10);
+
+            expect(countdown.state.count).toBe(10);
+            expect(countdown.state.countdownStatus).toBe('started');
+
+            setTimeout(() =>{
+                expect(countdown.state.count).toBe(9);
+                done();
+            }, 1001);
+        });
+
+        it('should not let count equal a negative number', (done)=>{
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            setTimeout(() =>{
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 3005);
+        });
+    });
 });
 
